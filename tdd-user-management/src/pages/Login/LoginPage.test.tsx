b39cf89eef78db6e7a52ad8336a406a50dbec93b
samirjabib <samirdev9669@gmail.com>
2023-03-14 21:21:26 -0500

    add

--- tdd-user-management/src/pages/Login/LoginPage.test.tsx
+++ tdd-user-management/src/pages/Login/LoginPage.test.tsx
@@ -1,18 +1,32 @@
 import { LoginPage } from "./LoginPage";
 
-import { screen, render } from '@testing-library/react' 
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
+test("it should render the login", () => {
+  render(<LoginPage />);
 
-test('it should render the login', () => {
-    render(<LoginPage/>)
+  expect(screen.getByRole("heading", { name: /login/i }));
+});
 
-    expect(screen.getByRole('heading', {name:/login/i}))
-})
+test("it should render form elements", () => {
+  render(<LoginPage />);
 
+  expect(screen.getByLabelText(/email/i)).toBeInTheDocument();
+  expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
+  expect(screen.getByRole("button", { name: /submit/i })).toBeInTheDocument();
+});
 
-test('it should render form elements', () => {
-    render(<LoginPage/>)
+test.only("it should validate the inputs as required", async() => { //La marcamos como asincrono por que estamos esperando un retorno en los datos del form. 
+  render(<LoginPage />);
 
-    expect(screen.getByLabelText(/email/i)).toBeInTheDocument()
-    expect(screen.getByLabelText(/password/i)).toBeInTheDocument()
-})
+  //submit form
+  userEvent.click(screen.getByRole("button", { name: /submit/i }));
+
+  expect(await screen.getByText(/the email is required/i)).toBeInTheDocument();
+  expect(
+    await screen.getByText(/the password is required/i)
+  ).toBeInTheDocument();
+
+  //expect validation errors
+});
--- tdd-user-management/src/pages/Login/LoginPage.tsx
+++ tdd-user-management/src/pages/Login/LoginPage.tsx
@@ -1,11 +1,29 @@
 import { Typography, TextField, Button } from "@mui/material";
+import "./LoginPage.styles.css";
+
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface Inputs {
+  email: string;
+  password: string;
+}
 
 export const LoginPage = () => {
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
-    <>
-      <Typography component="h1">Login</Typography>;
-      <TextField label="email" />;
-      <TextField label="password" />;
-    </>
+    <div className="login_container">
+      <Typography component="h1">Login</Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField label="email" {...register("email", { required: true })} />
+        <TextField
+          label="password"
+          {...register("password", { required: true })}
+        />
+        <Button>submit</Button>
+      </form>
+    </div>
   );
 };
