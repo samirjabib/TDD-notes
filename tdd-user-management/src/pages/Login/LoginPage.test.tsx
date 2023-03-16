068690018ab4ed6a4fae4bfc9b030c9719d05051
samirjabib <samirdev9669@gmail.com>
2023-03-15 21:32:41 -0500

    add

--- tdd-user-management/src/pages/Login/LoginPage.test.tsx
+++ tdd-user-management/src/pages/Login/LoginPage.test.tsx
@@ -3,8 +3,11 @@ import { LoginPage } from "./LoginPage";
 import { screen, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
+const getSubmitBtn = () => screen.getByRole('button', { name: /submit/i }) // get button submit for reuse in diferents test
+
+
 test("it should render the login", () => {
-  render(<LoginPage />);
+  render(<LoginPage />); //Render is a method for render componentet of react with the library
 
   expect(screen.getByRole("heading", { name: /login/i }));
 });
@@ -12,21 +15,34 @@ test("it should render the login", () => {
 test("it should render form elements", () => {
   render(<LoginPage />);
 
-  expect(screen.getByLabelText(/email/i)).toBeInTheDocument();
+  expect(screen.getByLabelText(/email/i)).toBeInTheDocument(); //optain input with the labeltext of label. this method tobeindocument is provided with json-dom
   expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
-  expect(screen.getByRole("button", { name: /submit/i })).toBeInTheDocument();
+  expect(screen.getByRole("button", { name: /submit/i })).toBeInTheDocument(); //we searh with the rol of label in this case this is button
 });
 
-test.only("it should validate the inputs as required", async() => { //La marcamos como asincrono por que estamos esperando un retorno en los datos del form. 
+test("it should validate the inputs as required", async () => {
+  //La marcamos como asincrono por que estamos esperando un retorno en los datos del form.
   render(<LoginPage />);
 
   //submit form
-  userEvent.click(screen.getByRole("button", { name: /submit/i }));
+  userEvent.click(getSubmitBtn()) //User event is a library alternative to fireEvent but better 
 
-  expect(await screen.getByText(/the email is required/i)).toBeInTheDocument();
+  expect(await screen.findByText(/The email is required/i)).toBeInTheDocument(); //the find method search of async way in the component for this i use async function. 
   expect(
-    await screen.getByText(/the password is required/i)
+    await screen.findByText(/The password is required/i)
   ).toBeInTheDocument();
-
   //expect validation errors
+});
+
+test("it should validate the email format ", async () => {
+  //La marcamos como asincrono por que estamos esperando un retorno en los datos del form.
+  render(<LoginPage />);
+
+  userEvent.type((screen.getByLabelText(/email/i)), 'invaliid email')  //search the event with the invalida email
+
+  //submit form
+  userEvent.click(getSubmitBtn()) //make a click in form XD
+
+  expect(await screen.findByText(/The email is not valid/i)).toBeInTheDocument();
+
 });
--- tdd-user-management/src/pages/Login/LoginPage.tsx
+++ tdd-user-management/src/pages/Login/LoginPage.tsx
@@ -1,29 +1,50 @@
 import { Typography, TextField, Button } from "@mui/material";
-import "./LoginPage.styles.css";
-
 import { useForm, SubmitHandler } from "react-hook-form";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const loginShema = yup
+  .object({
+    email: yup.string().required("The email is required"),
+    password: yup.string().required("The password is required/"),
+  })
+  .required();
 
 interface Inputs {
   email: string;
   password: string;
 }
 
-export const LoginPage = () => {
-  const { register, handleSubmit } = useForm<Inputs>();
+export function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({ resolver: yupResolver(loginShema) });
 
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   return (
-    <div className="login_container">
+    <>
       <Typography component="h1">Login</Typography>
+
       <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField label="email" {...register("email", { required: true })} />
         <TextField
-          label="password"
-          {...register("password", { required: true })}
+          label="Email"
+          {...register("email", { required: true })}
+          helperText={errors.email?.message}
         />
-        <Button>submit</Button>
+
+        <TextField
+          label="Password"
+          type="password"
+          {...register("password", { required: true })}
+          helperText={errors.password?.message}
+        />
+
+        <Button type="submit">Submit</Button>
       </form>
-    </div>
+    </>
   );
-};
+}
